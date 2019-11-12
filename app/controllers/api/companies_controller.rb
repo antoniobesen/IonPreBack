36026146bd41f6e14f2f32c24db34bf771039b48
antonio <antoniobesen@hotmail.com>
2019-11-12 13:02:28 -0300

    API CRUD users and companies completed

--- app/controllers/api/companies_controller.rb
+++ app/controllers/api/companies_controller.rb
@@ -3,5 +3,33 @@ module Api
         def index
             render json: Company.all, status: :ok
         end
+        def show
+            render json: Company.find(params[:id]), status: :ok
+        end
+        def create
+            company = Company.new(params.permit(:name))
+            if company.save
+                render json: company, status: :ok 
+            else render status: :unprocessable_entity #?
+            end
+        end
+        def update
+            company = Company.find(params[:id])
+            if company.update_attributes(params.permit(:name))
+                render json: company, status: :ok
+            else
+                render status: :unprocessable_entity #?
+            end
+        end
+        def destroy
+            company = Company.find(params[:id])
+            if company.destroy
+                render json: company, status: :ok
+            else
+                render status: :unprocessable_entity #?
+            end
+
+        end
+        
     end
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,8 +1,37 @@
 module Api
     class UsersController < ApplicationController
         def index
-            users = User.order('created_at DESC')
             render json: User.all, status: :ok
         end
+
+        def show
+            render json: User.find(params[:id]), status: :ok
+        end
+
+        def create
+            user = User.new(params.permit(:email, :company_id))
+            if user.save
+                render json: user, status: :ok 
+            else render status: :unprocessable_entity #?
+            end
+        end
+        def update
+            user = User.find(params[:id])
+            if user.update_attributes(params.permit(:email))
+                render json: user, status: :ok
+            else
+                render status: :unprocessable_entity #?
+            end
+        end
+        def destroy
+            user = User.find(params[:id])
+            if user.destroy
+                render json: user, status: :ok
+            else
+                render status: :unprocessable_entity #?
+            end
+
+        end
+
     end
 end
