a8a74e18041e59ed06d3ec127538aa0436c2dcb9
antonio <antoniobesen@hotmail.com>
2019-11-13 10:29:44 -0300

    New API routes working, csv file receiving not working properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
-  namespace 'api' do
-    resources :users
-  end
-    
-  namespace 'api' do
-    resources :companies
-  end
-
-  namespace 'api' do
-    resources :events
-    # post "events/import-csv", to: "events#import_csv"
-  end
-  
-
-  
+  resources :users
+  resources :companies
+  resources :events
+  post "events/import-csv", to: "events#import_csv"
 end
